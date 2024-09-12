1b9655a1ea4574fa61bf631f9160955887468923
Manav Chauhan <92674597+ManavChauhan1@users.noreply.github.com>
2024-09-12 21:59:37 +0530

    Basic setup complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const socket = require('eocket.io');
+const socket = require('socket.io');
 const http = require('http');
 const { Chess } = require('chess.js');
 const path = require('path');
